ffaab9c70dbfcf2f7b36b8987322af414bd11e03
Musquan Karovalia <71215436+Musquan@users.noreply.github.com>
2023-04-30 12:18:13 -0500

    Final Phase Update

---
+++ src/main/java/WordReplace.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class WordReplace {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the text:");
+        String text = scanner.nextLine();
+
+        System.out.println("Enter the word to replace:");
+        String pattern = scanner.nextLine();
+
+        System.out.println("Enter the replacement word:");
+        String replacement = scanner.nextLine();
+
+        // Replace all occurrences of the pattern word with the replacement word
+        text = text.replaceAll("\\b" + pattern + "\\b", replacement);
+
+        System.out.println("New String: " + text);
+    }
+}
+
+
